0a824246ffbe3b073b74ac86088ee15772990021
shaozongren <1244602518@qq.com>
2019-09-26 16:29:15 +0800

    没有登陆就不能进入home页

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -101,7 +101,14 @@
   </div>
 </template>
 <script>
-export default {};
+export default {
+    beforeCreate(){
+        const token=localStorage.getItem('token')
+        if(!token){
+            this.$router.push({name:'login'})
+        }
+    }
+};
 </script>
 <style>
 .home {
